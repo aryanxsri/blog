97dc7fb09be993102898a047ec10d5b14ef938fe
aryan <aryansri3112@gmail.com>
2024-09-25 00:14:29 +0530

    updated code

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,77 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { decode,sign, verify } from 'hono/jwt'
+import {signupinput} from "../../../common/index"
+
+
+
+export const userRouter = new Hono<{
+    Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET:string
+    }
+  }>()
+
+
+userRouter.post('/signup', async (c) => {
+    const body = await c.req.json()
+
+    const {success}=signupinput.safeParse(body)
+    if(!success){
+      c.status(411)
+      return c.json({
+        message:"Input not correct"
+      })
+    }
+
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    try {
+      const user=await prisma.user.create({
+        data: {
+          email: body.email,
+          password: body.password,
+          name: body.name
+        }
+      })
+      const jwt=await sign({
+        id:user.id
+      },c.env.JWT_SECRET)
+      return c.text(jwt)
+    } catch (e) {
+      c.status(411)
+      return c.text("invalid")
+    }
+  })
+  userRouter.post('/signin', async (c) => {
+  
+    const body = await c.req.json()
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    try {
+      const user=await prisma.user.findFirst({
+        where: {
+          email: body.email,
+          password: body.password
+        }
+      })
+      if(!user){
+        c.status(403)
+        return c.json({
+          message:"Incorrect credentials"
+        })
+      }
+      const jwt=await sign({
+        id:user.id
+      },c.env.JWT_SECRET)
+      return c.text(jwt)
+    } catch (e) {
+      c.status(411)  
+      return c.text("invalid")
+    }
+  })
---
+++ common/index.ts
@@ -0,0 +1,37 @@
+import z from "zod"
+
+export const signupinput=z.object({
+    username:z.string().email(),
+    password:z.string().min(6),
+    name:z.string().optional()
+  })
+
+  export type SignupInput=z.infer<typeof signupinput>
+
+
+
+  export const signininput=z.object({
+    username:z.string().email(),
+    password:z.string().min(6),
+    name:z.string().optional()
+  })
+
+  export type SigninInput=z.infer<typeof signininput>
+  
+
+
+  export const createbloginput=z.object({
+    title:z.string(),
+    content:z.string(),
+  })
+
+  export type CreateblogInput=z.infer<typeof createbloginput>
+
+
+  export const updatebloginput=z.object({
+    title:z.string(),
+    content:z.string(),
+    id:z.number()
+  })
+
+  export type UpdateblogInput=z.infer<typeof updatebloginput>
